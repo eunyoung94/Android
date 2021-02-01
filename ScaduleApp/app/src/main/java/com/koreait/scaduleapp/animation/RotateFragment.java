7641928453b5bf51c019d09b58720b5d02deea5e
DESKTOP-CELFE6I\choi6 <choi67sa@naver.com>
2021-02-01 23:34:20 +0900

    homework

--- ScaduleApp/app/src/main/java/com/koreait/scaduleapp/animation/RotateFragment.java
+++ ScaduleApp/app/src/main/java/com/koreait/scaduleapp/animation/RotateFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,8 +17,19 @@ import com.koreait.scaduleapp.R;
 
 //투명도를 실습하기 위한 페이지
 public class RotateFragment extends Fragment {
+    ImageView imageView;
+    Button bt_start;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rotate,container,false);
+        imageView = view.findViewById(R.id.imgView);
+        bt_start=view.findViewById(R.id.bt_start);
+        bt_start.setOnClickListener(e->{
+            showAni();
+        });
         return view;
+    }
+    public void showAni(){
+        Animation animation = AnimationUtils.loadAnimation(getContext(),R.anim.ani_rotate);
+        imageView.startAnimation(animation); //적용및 시작
     }
 }
--- ScaduleApp/app/src/main/java/com/koreait/scaduleapp/animation/TranslateFragment.java
+++ ScaduleApp/app/src/main/java/com/koreait/scaduleapp/animation/TranslateFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,8 +17,24 @@ import com.koreait.scaduleapp.R;
 
 //투명도를 실습하기 위한 페이지
 public class TranslateFragment extends Fragment {
+    ImageView imageView;
+    Button bt_start;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_translate,container,false);
+        imageView = view.findViewById(R.id.imgView);
+        bt_start=view.findViewById(R.id.bt_start);
+        //버튼을 누르면 이미지에 에니메이션 적용
+        bt_start.setOnClickListener(e->{
+            showAni();
+        });
         return view;
+    }
+    public void showAni(){
+        //에니메이션 객체 생성하기
+        Animation animation = AnimationUtils.loadAnimation(getContext(),R.anim.ani_translate);
+        //위의 에니메이션 객체를 누구에게?
+        imageView.startAnimation(animation); //적용및 시작!
+
     }
 }

9ff0a2ee34f99fed6c7aba4bc977a26a7bd51466
eunyoung94 <choi67sa@gmail.com>
2021-02-03 23:19:23 +0900

    -

---
+++ WebSocketClient/app/src/main/java/com/koreait/websocketclient/MyWebSocketClient.java
@@ -0,0 +1,69 @@
+package com.koreait.websocketclient;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URI;
+
+public class MyWebSocketClient extends WebSocketClient {
+
+    String TAG=this.getClass().getName();
+    MainActivity mainActivity;
+    Gson gson=new Gson();
+    public MyWebSocketClient(URI serverUri,MainActivity mainActivity) {
+        super(serverUri);
+        this.mainActivity=mainActivity;
+    }
+
+    //서버와 연결되면..
+    public void onOpen(ServerHandshake handshakedata) {
+        Log.d(TAG,"onOpen called");
+    }
+
+    //메세지가 도착하면
+    public void onMessage(String message) {
+        Log.d(TAG,"onMessage called"+message);
+        //서버에서 전달되어 온 메세지 분석
+        //서버에서 발생한 일이? 등록? 수정 ? 삭제?
+        try {
+            JSONObject json= new JSONObject(message);
+            if(json.get("requestCode").equals("create")){ //서버에서 등록작업이 누군가에 의해 발생함
+                // 목록을 갱신
+                mainActivity.boardDAO.selectAll();
+                Log.d(TAG,"글쓰기 발생");
+            }else if(json.get("requestCode").equals("update")){
+                mainActivity.boardDAO.selectAll();
+                Log.d(TAG,"수정 발생");
+            }else if(json.get("requestCode").equals("delete")){
+                mainActivity.boardDAO.selectAll();
+                Log.d(TAG,"삭제 발생");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //메세지 보내기
+    public void sendMsg(SocketMessage socketMessage){
+        String jsonString=gson.toJson(socketMessage);
+       this.send(jsonString);
+    }
+
+    //접속이 끊기면
+    public void onClose(int code, String reason, boolean remote) {
+        Log.d(TAG,"onClose called");
+    }
+
+    //에러가 발생하면
+    public void onError(Exception ex) {
+        Log.d(TAG,"onError called");
+    }
+}
